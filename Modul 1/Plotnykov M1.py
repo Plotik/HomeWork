01ea1ddf5b964ea1b5ece68125198f5ebfc093d1
Plotik <plotik13@gmail.com>
2021-09-05 18:20:09 +0300

    added str with programs and programs choice were solved

--- Modul 1/Plotnykov M1.py
+++ Modul 1/Plotnykov M1.py
@@ -10,45 +10,55 @@ else:
     print(file.read(), ", Hello! Nice To Meet You! ")
     file.close()
 
+
+program = ["1", "2", "3", "4", "q", "Q"]
 print("We have a few program for you: ")
-print("\033[31mProgram #1 -   BRICKS   - Enter 1")
-print("\033[32mProgram #2 - DICTIONARY - Enter 2")
-print("\033[33mProgram #3 -  SEQUENCE  - Enter 3")
-print("\033[34mProgram #4 - ChangeName - Enter 4")
-print("\033[35mProgram #5 -    QUIT    - Enter q")
+while True:
+    print("\033[37m=================================")
+    print("\033[31mProgram #1 -   BRICKS   - Enter 1")
+    print("\033[32mProgram #2 - DICTIONARY - Enter 2")
+    print("\033[33mProgram #3 -  SEQUENCE  - Enter 3")
+    print("\033[34mProgram #4 - ChangeName - Enter 4")
+    print("\033[35mProgram #5 -    QUIT    - Enter q")
+    print("\033[37m=================================")
+    x = input("\033[38mMake your choice: ")
+    choice = x.lower()
 
-choice = input("\033[38mMake your choice: ")
+    # ========= PROGRAM 1
+    if choice not in program:
+        print("--> Please enter correct number")
 
-# ========= PROGRAM 1
-if ord(choice) == 49:
-    import WALL
-    print("\033[31mProgram 1 - Answer: ")
-    res = WALL.breaks_solution(WALL.wall)
-    if res != 0:
-        print("Наименьшее количество пересечений кирпичей:", res)
-    else:
-        print("Пересечений кирпичей нет")
+    if choice == "1":
+        import WALL
+        print("\033[31mProgram 1 - Answer: ")
+        res = WALL.breaks_solution(WALL.wall)
+        if res != 0:
+            print("Наименьшее количество пересечений кирпичей:", res)
+        else:
+            print("Пересечений кирпичей нет")
 
-# ========= PROGRAM 2
-elif ord(choice) == 50:
-    print("\033[32mProgram 2 - Answer: ")
-    import SEQUENCE
-    for norm_view in SEQUENCE.get_uniq(SEQUENCE.data, SEQUENCE.keys):
-        print(norm_view)
-# ========= PROGRAM 3
-elif ord(choice) == 51:
-    print("\033[32mProgram 3 - Answer: ")
-    import SORT
-    print(SORT.data)
-# ========= PROGRAM 4
-elif ord(choice) == 52:
-    file = (open("profile.txt", "w", encoding="utf-8"))
-    profile_name = input("Put Your New Name: ")
-    file.write(profile_name)
-    print("Your name has been changed to\033[31m\033[1m", profile_name)
-    file.close()
-# ========= PROGRAM 5
-elif ord(choice) == 113:
-    print("Your Program Was Ended")
-else:
-    print("Please, Select Correct Program")
+    # ========= PROGRAM 2
+    if choice == "2":
+        print("\033[32mProgram 2 - Answer: ")
+        import SEQUENCE
+        for norm_view in SEQUENCE.get_uniq(SEQUENCE.data, SEQUENCE.keys):
+            print(norm_view)
+
+    # ========= PROGRAM 3
+    if choice == "3":
+        print("\033[32mProgram 3 - Answer: ")
+        import SORT
+        print(SORT.data)
+
+    # ========= PROGRAM 4
+    if choice == "4":
+        file = (open("profile.txt", "w", encoding="utf-8"))
+        profile_name = input("Put Your New Name: ")
+        file.write(profile_name)
+        print("Your name has been changed to\033[31m\033[1m", profile_name)
+        file.close()
+
+    # ========= PROGRAM 5
+    if choice == "q":
+        print("Your Program Was Ended")
+        break
